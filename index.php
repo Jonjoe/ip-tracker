32816262db2403ff5780073dc0ab4042dd9a747c
Jonjoe Whitfield <me@jonjoe.io>
2014-11-30 23:24:53 +0000

    build

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
-	$ipaddress = $_SERVER["REMOTE_ADDR"];
+	$current = file_get_contents("ips.txt");
+	$i = $_SERVER["REMOTE_ADDR"];
+	$output = $current.$i." | ";
+	$f = fopen('ips.txt', 'w');
+	fwrite($f, $output);
+	fclose($f);
 
-	echo "Your IP is $ipaddress!";
+
+
+	if (is_writable("ips.txt")) {
+		echo "true";
+	} else {
+		echo "false";
+	}
+	// echo "Your IP is $ipaddress!";
